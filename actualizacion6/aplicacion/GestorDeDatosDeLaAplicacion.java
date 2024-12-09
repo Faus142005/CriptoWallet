7d09b9e697782d0c32ab541504075a54ed913fa8
Faus142005 <rodriguezfausto2005@gmail.com>
2024-12-09 15:22:46 -0300

    Add files via upload

--- actualizacion6/aplicacion/GestorDeDatosDeLaAplicacion.java
+++ actualizacion6/aplicacion/GestorDeDatosDeLaAplicacion.java
@@ -1,10 +1,12 @@
 package aplicacion;
 
+import clases.Criptomoneda;
 import clases.Usuario;
 
 public class GestorDeDatosDeLaAplicacion {
 
-	private static Usuario usuarioConectado = new Usuario(1, null, null, null, false);
+	private static Usuario usuarioConectado = null;
+	private static Criptomoneda criptomonedaSeleccionada = new Criptomoneda(-1, null, null, 1, null, 1);
 	/**
 	 * @return the usuarioConectado
 	 */
@@ -17,4 +19,18 @@ public class GestorDeDatosDeLaAplicacion {
 	public static void setUsuarioConectado(Usuario usuarioConectado) {
 		GestorDeDatosDeLaAplicacion.usuarioConectado = usuarioConectado;
 	}
+	/**
+	 * @return the criptomonedaSeleccionada
+	 */
+	public static Criptomoneda getCriptomonedaSeleccionada() {
+		return criptomonedaSeleccionada;
+	}
+	/**
+	 * @param criptomonedaSeleccionada the criptomonedaSeleccionada to set
+	 */
+	public static void setCriptomonedaSeleccionada(Criptomoneda criptomonedaSeleccionada) {
+		GestorDeDatosDeLaAplicacion.criptomonedaSeleccionada = criptomonedaSeleccionada;
+	}
+	
+	
 }
